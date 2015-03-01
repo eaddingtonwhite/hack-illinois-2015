fae44f9b93935767f421311ac44e9473a7bbab89
ellery44 <addingtonwhiteem@beloit.edu>
2015-02-28 23:17:24 -0600

    adds 2nd client

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 var express = require('express'),
-app = express(),
-server = require('http').createServer(app),
-io = require('socket.io').listen(server);
+  app = express(),
+  server = require('http').createServer(app),
+  io = require('socket.io').listen(server);
 
 server.listen(9030);
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+// GET /
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-io.sockets.on('connection', function (socket) {
+// GET /client2
+app.get('/client2', function(req, res) {
+  res.sendfile(__dirname + '/public/client2.html');
+})
+io.sockets.on('connection', function(socket) {
 
-    socket.on('send frame', function (data) {
-       console.log(data);
+  socket.on('send frame', function(data) {
+    console.log(data);
+    io.sockets.emit('recieve frame', {
+      imgFrame: data
+    });
 
-   });
+  });
 
 });
